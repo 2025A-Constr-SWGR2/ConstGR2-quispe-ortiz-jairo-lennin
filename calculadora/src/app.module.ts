5d1948a7889f6305f46b375fe174e26250d52e1d
Jairo-lo <jairo.quispe@epn.edu.ec>
2025-08-03 01:48:19 -0500

    Repaired

--- calculadora/src/app.module.ts
+++ calculadora/src/app.module.ts
@@ -1,29 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: 'localhost',
-        port: configService.get<number>('POSTGRES_PORT', 5432),
-        username: configService.get<string>('POSTGRES_USER'),
-        password: configService.get<string>('POSTGRES_PASSWORD'),
-        database: configService.get<string>('POSTGRES_DB'),
-        autoLoadEntities: true,
-        synchronize: true, // Solo para desarrollo
-      }),
-      inject: [ConfigService],
-    }),
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
-
