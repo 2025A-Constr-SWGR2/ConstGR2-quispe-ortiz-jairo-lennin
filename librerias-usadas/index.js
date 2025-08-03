abbe3cdb4c27482336c4faabc31c15e0dacb8c7a
Jairo-lo <jairo.quispe@epn.edu.ec>
2025-08-03 05:06:22 -0500

    Examen 2B - Parámetros modificados

--- librerias-usadas/index.js
+++ librerias-usadas/index.js
@@ -1,15 +1,18 @@
+// MODIFICADO PARA EL EXAMEN.
+// Autores: Jairo Quispe y René Guzmán.
+
 const moduloSuma = require('2025a-swgr2-jq-rg-suma');
 const moduloResta = require('2025a-swgr2-jq-rg-resta');
 const moduloMultiplicacion = require('2025a-swgr2-jq-rg-multiplicacion');
 const moduloDivision = require('2025a-swgr2-jq-rg-division');
 
-const resultadoSuma = moduloSuma.suma(1, 4);
-const resultadoResta = moduloResta.resta(9, 2);
-const resultadoMultiplicacion = moduloMultiplicacion.multiplicacion(5, 3);
-const resultadoDivision = moduloDivision.division(6, 2);
+const resultadoSuma = moduloSuma.suma(2, 1);
+const resultadoResta = moduloResta.resta(3, 2);
+const resultadoMultiplicacion = moduloMultiplicacion.multiplicacion(2, 5);
+const resultadoDivision = moduloDivision.division(9, 3);
 
-console.log('Resultado suma:', resultadoSuma); // Resultado suma: 5
-console.log('Resultado resta:', resultadoResta); // Resultado resta: 7
-console.log('Resultado multiplicación:', resultadoMultiplicacion); // Resultado multiplicación: 15
+console.log('Resultado suma:', resultadoSuma); // Resultado suma: 3
+console.log('Resultado resta:', resultadoResta); // Resultado resta: 1
+console.log('Resultado multiplicación:', resultadoMultiplicacion); // Resultado multiplicación: 10
 console.log('Resultado división:', resultadoDivision); // Resultado división: 3
 
